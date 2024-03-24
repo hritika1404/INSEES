1845e28c360b0677fa37a4656c9ce7be6c3d39f4
Bishal <bishalp.biswas@gmail.com>
2024-03-24 13:25:03 +0530

    Backend integrated in Todo Task Page

--- app/src/main/java/com/example/insees/Fragments/SemesterFragment.kt
+++ app/src/main/java/com/example/insees/Fragments/SemesterFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.insees.R
 import com.example.insees.databinding.FragmentSemesterBinding
 
-
 class SemesterFragment : Fragment() {
+
+    private lateinit var parentRecyclerView: RecyclerView
     private lateinit var binding:FragmentSemesterBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +27,15 @@ class SemesterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        parentRecyclerView=view.findViewById(R.id.parentRecyclerView)
+        parentRecyclerView.setHasFixedSize(true)
+        parentRecyclerView.layoutManager=LinearLayoutManager(context)
+
+
+
+        binding.btnSemesterBack.setOnClickListener {
+            findNavController().navigate(R.id.action_semesterFragment_to_todoFragment)
+        }
 
     }
 
---
+++ app/src/main/java/com/example/insees/Utils/ToDoData.kt
@@ -0,0 +1,7 @@
+package com.example.insees.Utils
+
+data class ToDoData(var taskTitle:String,
+                    var taskDesc:String,
+                    var taskTime:String,
+                    var taskDate:String
+)
