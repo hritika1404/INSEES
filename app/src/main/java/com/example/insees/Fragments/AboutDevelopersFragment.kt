13a3363c9409638ec62b766fc93ebf024812b65b
Rishi Gorai <franistine384@gmail.com>
2024-06-02 11:02:16 +0530

    fragment names changed, About us section has toggle button, fixed heading alignments, gradle updated 8.2.0 -> 8.3.1

---
+++ app/src/main/java/com/example/insees/Fragments/AboutDevelopersFragment.kt
@@ -0,0 +1,26 @@
+package com.example.insees.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.insees.databinding.FragmentAboutDevelopersBinding
+
+class AboutDevelopersFragment : Fragment() {
+
+    private lateinit var binding: FragmentAboutDevelopersBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding=FragmentAboutDevelopersBinding.inflate(inflater,container,false)
+
+        return binding.root
+
+    }
+
+
+}
--- app/src/main/java/com/example/insees/Fragments/AboutMembersFragment.kt
+++ app/src/main/java/com/example/insees/Fragments/AboutMembersFragment.kt
@@ -1,26 +1,24 @@
 package com.example.insees.Fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.insees.databinding.FragmentAboutMembersBinding
 
+
+
 class AboutMembersFragment : Fragment() {
+    lateinit var binding: FragmentAboutMembersBinding
 
-    private lateinit var binding: FragmentAboutMembersBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
-        binding=FragmentAboutMembersBinding.inflate(inflater,container,false)
+        binding = FragmentAboutMembersBinding.inflate(inflater, container, false)
+        val view = binding.root
 
-        return binding.root
+        return view
 
     }
-
-
 }
