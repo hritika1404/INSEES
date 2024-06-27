6c56fcd928d15208adea4ce27096d9f81688a057
AnKiTk203 <kumar0ankit2003@gmail.com>
2024-06-27 13:09:42 +0530

    Major Bug and Navigation Fixes

--- app/src/main/java/com/example/insees/Fragments/ViewPagerFragment.kt
+++ app/src/main/java/com/example/insees/Fragments/ViewPagerFragment.kt
@@ -8,21 +8,20 @@ import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
-import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.example.insees.Adapters.ViewPagerAdapter
 import com.example.insees.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-
 class ViewPagerFragment : Fragment() {
 
-    private lateinit var viewPager : ViewPager2
+    private lateinit var viewPager: ViewPager2
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        val view =  inflater.inflate(R.layout.fragment_view_pager, container, false)
+        val view = inflater.inflate(R.layout.fragment_view_pager, container, false)
 
         val fragmentList = arrayListOf(
             HomeFragment(),
@@ -30,38 +29,36 @@ class ViewPagerFragment : Fragment() {
             TodoFragment(),
             InseesAboutUsFragment()
         )
-        
+
         val adapter = ViewPagerAdapter(fragmentList, childFragmentManager, lifecycle)
-        viewPager = view.findViewById<ViewPager2>(R.id.viewPager)
+        viewPager = view.findViewById(R.id.viewPager)
         viewPager.adapter = adapter
 
-        val bottomNavigationView = requireActivity().findViewById<BottomNavigationView>(R.id.bvNavBar)
+        bottomNavigationView = requireActivity().findViewById(R.id.bvNavBar)
 
-        viewPager.registerOnPageChangeCallback(object : OnPageChangeCallback() {
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
                 bottomNavigationView.menu.getItem(position).isChecked = true
             }
         })
+
         bottomNavigationView.setOnItemSelectedListener { item: MenuItem ->
             when (item.itemId) {
                 R.id.homeFragment -> {
-                    viewPager.currentItem = 0
+                    setViewPagerItem(0)
                     true
                 }
-
                 R.id.todoFragment -> {
-                    viewPager.currentItem = 2
+                    setViewPagerItem(2)
                     true
                 }
-
                 R.id.semesterFragment -> {
-                    viewPager.currentItem = 1
+                    setViewPagerItem(1)
                     true
                 }
-
                 R.id.inseesAboutUsFragment -> {
-                    viewPager.currentItem = 3
+                    setViewPagerItem(3)
                     true
                 }
                 else -> false
@@ -74,13 +71,17 @@ class ViewPagerFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val callback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                if(viewPager.currentItem != 0)
-                viewPager.currentItem = 0
-                else{
+                if (viewPager.currentItem != 0) {
+                    setViewPagerItem(0)
+                } else {
                     requireActivity().finish()
                 }
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+    }
+
+    private fun setViewPagerItem(position: Int) {
+        viewPager.setCurrentItem(position, false)
     }
 }
